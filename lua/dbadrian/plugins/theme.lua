3a1ad1a0d1c8a667b7c1d3389b066e2d16e5394a
Adrian David (CR/AAS2) <david.adrian@de.bosch.com>
2025-09-05 08:27:40 +0200

    more refactoring

--- lua/dbadrian/plugins/theme.lua
+++ lua/dbadrian/plugins/theme.lua
@@ -13,8 +13,7 @@ return {
           comments = { italic = false }, -- Disable italics in comments
         },
       }
-
-      -- Load the colorscheme here.
+            -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.cmd.colorscheme 'tokyonight-night'
