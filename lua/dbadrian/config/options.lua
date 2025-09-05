0b31b04ffd36ad8373c0f0e60f66a30220c75b36
Adrian David (CR/AAS2) <david.adrian@de.bosch.com>
2025-09-05 08:50:05 +0200

    additional opts:

--- lua/dbadrian/config/options.lua
+++ lua/dbadrian/config/options.lua
@@ -1,22 +1,10 @@
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
-
--- [[ Setting options ]]
--- See `:help vim.o`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
-
--- Make line numbers default
+vim.o.title = true
 vim.o.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
@@ -79,3 +67,11 @@ vim.o.scrolloff = 10
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
 vim.o.confirm = true
+
+vim.o.wrap = true
+
+-- indentation
+vim.o.smarttab = true
+vim.o.cindent = true
+vim.o.tabstop = 4
+vim.o.autoindent = false
